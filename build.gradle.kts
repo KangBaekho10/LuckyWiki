72fbdc92d525762177ada971470bb2cfd3da825c
eunhye han <hellou8363@gmail.com>
2024-07-03 07:13:07 +0900

    Update: 테스트를 위한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,9 @@ allOpen {
     annotation("jakarta.persistence.Embeddable")
 }
 
+val kotestVersion = "5.5.5"
+val mockkVersion = "1.13.8"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -51,6 +54,13 @@ dependencies {
 
     // MySQL
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // TEST
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
 }
 
 kotlin {
