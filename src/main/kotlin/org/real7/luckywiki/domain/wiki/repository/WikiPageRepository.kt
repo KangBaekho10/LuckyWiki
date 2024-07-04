1b3fe9c78b67bdec9c0511f7464bfc1b17f529c5
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-04 15:42:54 +0900

    Feat: 랜덤 돌리기 위해서 가장 큰 아이디 찾음

--- src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/WikiPageRepository.kt
+++ src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/WikiPageRepository.kt
@@ -2,6 +2,10 @@ package org.real7.luckywiki.domain.wiki.repository
 
 import org.real7.luckywiki.domain.wiki.model.WikiPage
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface WikiPageRepository : JpaRepository<WikiPage, Long> {
+
+    @Query("SELECT MAX(w.id) FROM WikiPage w")
+    fun findMaxId(): Long?
 }
