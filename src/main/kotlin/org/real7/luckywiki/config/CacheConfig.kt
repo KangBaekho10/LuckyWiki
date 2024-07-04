9d3da2e5aa8cc5dc0ddfeea9e3820d1aa54a7922
eunhye han <hellou8363@gmail.com>
2024-07-05 00:26:28 +0900

    Add: Aop, Cache, Scheduler Config 클래스 추가

---
+++ src/main/kotlin/org/real7/luckywiki/config/CacheConfig.kt
@@ -0,0 +1,27 @@
+package org.real7.luckywiki.config
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.concurrent.ConcurrentMapCache
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+@EnableCaching
+class CacheConfig {
+
+    @Bean
+    fun cacheManager(): CacheManager {
+        val simpleCacheManager = SimpleCacheManager()
+        simpleCacheManager.setCaches(
+            listOf(
+                ConcurrentMapCache("popularWordTop10"),
+                ConcurrentMapCache("wikiPageList"),
+                ConcurrentMapCache("wikiPage")
+            )
+        )
+        return simpleCacheManager
+    }
+}
