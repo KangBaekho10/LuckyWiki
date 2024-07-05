cf26227ffd1bbc670574339231c2203061e31fa9
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-05 10:39:07 +0900

    Style: 이름 고침 주석

--- src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/WikiPageRepository.kt
+++ src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/WikiPageRepository.kt
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface WikiPageRepository : JpaRepository<WikiPage, Long> {
-
-    @Query("SELECT MAX(w.id) FROM WikiPage w")
-    fun findMaxId(): Long?
 }
