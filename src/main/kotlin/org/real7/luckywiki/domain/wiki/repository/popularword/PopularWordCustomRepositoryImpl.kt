a27b4fc19b8147f1687958bfbf8d74ecb9db9636
eunhye han <hellou8363@gmail.com>
2024-07-05 11:20:45 +0900

    Add: 24시간이 지난 검색어 데이터 삭제 스케줄러 구현

--- src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/popularword/PopularWordCustomRepositoryImpl.kt
+++ src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/popularword/PopularWordCustomRepositoryImpl.kt
@@ -3,6 +3,8 @@ package org.real7.luckywiki.domain.wiki.repository.popularword
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.real7.luckywiki.domain.wiki.model.QPopularWord
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Repository
 class PopularWordCustomRepositoryImpl(
@@ -18,5 +20,12 @@ class PopularWordCustomRepositoryImpl(
             .limit(10)
 
         return subQuery.select(popularWord.searchWord).fetch()
+    }
+
+    @Transactional
+    override fun deleteDataPast24Hours() {
+        queryFactory.delete(popularWord)
+            .where(popularWord.searchedAt.lt(LocalDateTime.now().minusHours(24)))
+            .execute()
     }
 }
