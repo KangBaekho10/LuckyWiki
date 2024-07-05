d6366dece8f3dee6265c4a1b6405ac265e999157
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-05 10:40:15 +0900

    Style: 이름 고침 주석

--- src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/WikiPageRepository.kt
+++ src/main/kotlin/org/real7/luckywiki/domain/wiki/repository/WikiPageRepository.kt
@@ -2,8 +2,6 @@ package org.real7.luckywiki.domain.wiki.repository
 
 import org.real7.luckywiki.domain.wiki.model.WikiPage
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface WikiPageRepository : JpaRepository<WikiPage, Long> {
-
 }
